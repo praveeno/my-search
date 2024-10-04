36827dba694d90694724a7388dcf9027da841738
praveeno.pro <praveeno.pro@gmail.com>
2024-10-04 05:55:14 +0000

    add more options

---
+++ src/app/search/search-options.ts
@@ -0,0 +1,50 @@
+import { query } from "@angular/animations";
+
+interface options {
+    name: string, 
+    query: (query: string) => string,
+    children?: options[] 
+}
+
+const site = (url: string) => `site:${url}`;
+const option = (name: string, url: string | string[]) => 
+        ({name, query: (query: string) => (Array.isArray(url) ? url.map(u => site(u)).join(' | '): site(url)) + ` ${query}`});
+
+export const options: options[] = [
+    {
+        name: "Obsidian Notes",
+        query: query => `inurl:publish.obsidian.md/* "${query}"`
+    },
+    {
+        name: 'Notion handbook',
+        query: (query) => `site:*.notion.site/* "Handbook" ${query}`
+    },
+    {
+        name:  "Job Search",
+        query(query) {
+            return (this.children?.map(c => c.query('')).join(' | ') + ' ' + query) as string;
+        },
+        children: [
+            option('jooble', 'jooble.org'),
+            option('lever', 'lever.co'),
+            option('greenhouse', ['greenhouse.io', 'greenhouse.com']),
+            option('join', 'join.com/*/*'),
+            option('personio', 'personio.com'),
+            option('ashbyhq', 'jobs.ashbyhq.com/*'),
+            option('turbohire', '*.turbohire.co'),
+            option('workable', 'apply.workable.com/*'),
+            option('jointaro jobs', 'www.jointaro.com/jobs/*/*'),
+            option('applytojob', '*.applytojob.com/apply'),
+            {name: 'page which us indeed widget', query: query => `"Apply with indeed" ${query}`},
+            option('traffit', '*.traffit.com/public/an/*'),
+            option('teamtailor', '*.teamtailor.com/jobs/*'),
+            option('euremotejobs', 'euremotejobs.com/job/*'),
+            option('flexa careers', 'flexa.careers/jobs/*'),
+            option('zoho recruit','*.zohorecruit.com/jobs/Careers/*/'),
+            option('bamboohr', '*.bamboohr.com/careers/*'),
+            option('pinpointhq', '*.pinpointhq.com/en/postings/*'),
+            option('recruitee', '*.recruitee.com'),
+            {name: 'myworkdayjobs', query: query => `myworkdayjobs ${query}`},
+        ]
+    }
+];
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { options } from './search-options';
 
 @Component({
   selector: 'app-search',
@@ -8,33 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  options = ["Obsidian Notes", "Job Search"];
+  options = options;
 
   searchGoogle(option: string, query: string) {
-    const encodedQuery = encodeURIComponent(getQueryBasedOnOptions(option, query));
+    const encodedQuery = encodeURIComponent(search(options, option, query));
     const googleUrl = `https://www.google.com/search?q=${encodedQuery}`;
     window.open(googleUrl, '_blank');
   }
 }
 
-function getQueryBasedOnOptions(option: string, query: string) {
-  switch (option) {
-    case "Obsidian Notes":
-      return `inurl:publish.obsidian.md/* "${query}"`;
-    case "Job Search":
-      const date = new Date();
-      date.setDate(1);
-      const firstDayOfMonth = date.toISOString().slice(0, 10);
-      const sites = [
-        "jooble.org", "lever.co", "greenhouse.io", 
-        "join.com", "personio.com", "personio.de",
-        "glassdoor.com", "uk.indeed.com",
-        "jobs.ashbyhq.com",
-        "jobs.*.com/*"
-      ].map(x => `site:http://${x}`).join(" | ").concat(' | site:https://*.turbohire.co')
-      return sites + `(developer | engineer) "remote" "${query}" after:${firstDayOfMonth}`
-    default:
-      return query;
+function search(options: any, selection: any, query: string): any {
+  for (const option of options) {
+    console.log(option)
+    if (option.name === selection) {
+      return option.query(query);
+    }
+    if (option.children) {
+      const result = search(option.children, selection, query);
+      if (result) {
+        return result;
+      }
+    }
   }
+  return null;
 }
-
